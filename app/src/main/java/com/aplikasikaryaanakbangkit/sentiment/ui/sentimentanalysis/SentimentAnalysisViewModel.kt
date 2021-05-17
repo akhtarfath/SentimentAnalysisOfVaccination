b3b0dc7ec653e2c6e62ad73c77d5aee2a6d45bd2
Muhammad Fathan A <muhammadfathan_a@uhamka.ac.id>
2021-05-18 01:37:56 +0700

    fix: default name [ui/home|ui/statistic|ui/sentimentanalysis]

--- app/src/main/java/com/aplikasikaryaanakbangkit/sentiment/ui/sentimentanalysis/SentimentAnalysisViewModel.kt
+++ app/src/main/java/com/aplikasikaryaanakbangkit/sentiment/ui/sentimentanalysis/SentimentAnalysisViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class SentimentAnalysisViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is Sentiment Analysis page"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/aplikasikaryaanakbangkit/sentiment/ui/statistic/StatisticViewModel.kt
+++ app/src/main/java/com/aplikasikaryaanakbangkit/sentiment/ui/statistic/StatisticViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class StatisticViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is Statistic page"
     }
     val text: LiveData<String> = _text
 }
