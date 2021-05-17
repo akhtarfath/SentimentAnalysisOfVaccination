e8a401955a5a0c461b7b5d163163d4bd2b7f1555
Muhammad Fathan A <muhammadfathan_a@uhamka.ac.id>
2021-05-18 01:17:44 +0700

    feat: create splashscreen [ui/splashscreen]

---
+++ app/src/main/java/com/aplikasikaryaanakbangkit/sentiment/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.aplikasikaryaanakbangkit.sentiment.ui.splashscreen
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.aplikasikaryaanakbangkit.sentiment.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
