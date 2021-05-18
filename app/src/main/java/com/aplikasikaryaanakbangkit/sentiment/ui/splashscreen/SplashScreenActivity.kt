aa7fc20447c7091733d4d7c71711059a44aee99d
ShobrinaFathoniah <shobrinaf@gmail.com>
2021-05-18 20:56:26 +0700

    fix: update splash screen feature

--- app/src/main/java/com/aplikasikaryaanakbangkit/sentiment/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/aplikasikaryaanakbangkit/sentiment/ui/splashscreen/SplashScreenActivity.kt
@@ -1,12 +1,21 @@
 package com.aplikasikaryaanakbangkit.sentiment.ui.splashscreen
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import com.aplikasikaryaanakbangkit.sentiment.MainActivity
 import com.aplikasikaryaanakbangkit.sentiment.R
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler(mainLooper).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3500)
     }
 }
