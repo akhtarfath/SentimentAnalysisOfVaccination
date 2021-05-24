5420865c2ae4dd52d06610b4bcc89bebc8db6fca
Muhammad Fathan A <muhammadfathan_a@uhamka.ac.id>
2021-05-24 08:21:19 +0700

    fix: update LocalDataSource

---
+++ app/src/main/java/com/aplikasikaryaanakbangkit/sentiment/core/data/source/local/LocalDataSource.kt
@@ -0,0 +1,41 @@
+package com.aplikasikaryaanakbangkit.sentiment.core.data.source.local
+
+import androidx.lifecycle.LiveData
+import androidx.paging.DataSource
+import com.aplikasikaryaanakbangkit.sentiment.core.data.source.local.entity.ArticleCovidEntity
+import com.aplikasikaryaanakbangkit.sentiment.core.data.source.local.entity.ArticleVaccinesEntity
+import com.aplikasikaryaanakbangkit.sentiment.core.data.source.local.room.NewsDao
+
+class LocalDataSource private constructor(
+    private val newsDao: NewsDao
+) {
+
+    companion object {
+        private var instance: LocalDataSource? = null
+
+        fun getInstance(
+            newsDao: NewsDao
+        ): LocalDataSource =
+            instance ?: synchronized(this) {
+                instance ?: LocalDataSource(newsDao)
+            }
+    }
+
+    fun getCovidArticles(): DataSource.Factory<Int, ArticleCovidEntity> =
+        newsDao.getCovidArticles()
+
+    fun insertCovidArticles(article: List<ArticleCovidEntity>) =
+        newsDao.insertCovidArticles(article)
+
+    fun getCovidArticleByUrl(url: String): LiveData<ArticleCovidEntity> =
+        newsDao.getCovidArticleByUrl(url)
+
+    fun getVaccineArticles(): DataSource.Factory<Int, ArticleVaccinesEntity> =
+        newsDao.getVaccineArticles()
+
+    fun insertVaccineArticles(article: List<ArticleVaccinesEntity>) =
+        newsDao.insertVaccineArticles(article)
+
+    fun getVaccineArticleByUrl(url: String): LiveData<ArticleVaccinesEntity> =
+        newsDao.getVaccineArticleByUrl(url)
+}
