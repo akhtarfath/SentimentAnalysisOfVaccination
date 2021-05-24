c92683c0b6d35382f65195baabc0de6f8994c7f3
Muhammad Fathan A <muhammadfathan_a@uhamka.ac.id>
2021-05-24 08:22:50 +0700

    feat: add ApiResponse.kt

---
+++ app/src/main/java/com/aplikasikaryaanakbangkit/sentiment/core/data/source/remote/network/ApiResponse.kt
@@ -0,0 +1,15 @@
+package com.aplikasikaryaanakbangkit.sentiment.core.data.source.remote.network
+
+import com.aplikasikaryaanakbangkit.sentiment.core.data.source.remote.StatusResponse
+
+class ApiResponse<T>(val status: StatusResponse, val body: T, val message: String?) {
+    companion object {
+        fun <T> success(body: T): ApiResponse<T> = ApiResponse(StatusResponse.SUCCESS, body, null)
+
+        fun <T> empty(msg: String, body: T): ApiResponse<T> =
+            ApiResponse(StatusResponse.EMPTY, body, msg)
+
+        fun <T> error(msg: String, body: T): ApiResponse<T> =
+            ApiResponse(StatusResponse.ERROR, body, msg)
+    }
+}
