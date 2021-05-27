8dc630b722c9e318352a1793d80a294094bf327a
Muhammad Fathan A <muhammadfathan_a@uhamka.ac.id>
2021-05-27 20:01:49 +0700

    fix: sama aja

--- app/src/main/java/com/aplikasikaryaanakbangkit/sentiment/sentiment/SentimentAnalysisViewModel.kt
+++ app/src/main/java/com/aplikasikaryaanakbangkit/sentiment/sentiment/SentimentAnalysisViewModel.kt
@@ -1,21 +1,17 @@
 package com.aplikasikaryaanakbangkit.sentiment.sentiment
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.aplikasikaryaanakbangkit.sentiment.core.data.SAVRepository
-import com.aplikasikaryaanakbangkit.sentiment.core.data.source.local.entity.DataItemTweetEntity
-import com.aplikasikaryaanakbangkit.sentiment.core.data.source.local.entity.TweetEntity
-import com.aplikasikaryaanakbangkit.sentiment.core.data.source.local.entity.UserItemsTweetEntity
-import com.aplikasikaryaanakbangkit.sentiment.core.vo.Resource
 
 class SentimentAnalysisViewModel(
     private val _repository: SAVRepository
 ) : ViewModel() {
 
-    fun getTweet(): LiveData<List<TweetEntity>> = _repository.getAllTweet()
+    fun getTweet() = _repository.getAllTweet()
 
-    fun getPost(): LiveData<Resource<List<DataItemTweetEntity>>> = _repository.getAllPost()
-    fun getProfile(): LiveData<Resource<List<UserItemsTweetEntity>>> = _repository.getAllProfile()
-    // fun getMetrics(): LiveData<Resource<List<PublicMetricsTweetEntity>>> = _repository.()
+    fun getPost() = _repository.getAllPost()
 
+    fun getProfile() = _repository.getAllProfile()
+
+    fun getMetrics(id: String) = _repository.getPublicMetrics(id)
 }
